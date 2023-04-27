b885c11ee25ccfb2d728379a48af3294f54fb292
introidx <prakash.r19500@gmail.com>
2023-04-27 20:16:47 +0530

    more code

--- src/test/java/interview/SinchInterviewTest.java
+++ src/test/java/interview/SinchInterviewTest.java
@@ -9,20 +9,20 @@ class SinchInterviewTest {
     SinchInterview sinchInterview;
 
 
-    @Test
-    void reverseNode() {
-        Node head = new Node(1);
-        Node a = new Node(2);
-        Node b = new Node(3);
-        Node c = new Node(4);
-        Node d = new Node(5);
-
-        head.next = a;
-        a.next=b;
-        b.next=c;
-        c.next=d;
-
-
-        assertEquals(d.val, SinchInterview.reverseNodes(head).val);
-    }
+//    @Test
+//    void reverseNode() {
+//        Node head = new Node(1);
+//        Node a = new Node(2);
+//        Node b = new Node(3);
+//        Node c = new Node(4);
+//        Node d = new Node(5);
+//
+//        head.next = a;
+//        a.next=b;
+//        b.next=c;
+//        c.next=d;
+//
+//
+//        assertEquals(d.val, SinchInterview.reverseNodes(head).val);
+//    }
 }
